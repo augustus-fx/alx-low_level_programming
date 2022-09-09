4feb2603e228dd9bc298c498bbbec6d2be4ae845
augustus-fx <agbolanaugustine@gmail.com>
2022-09-09 03:24:38 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - entry point
  *
- * Description: using the main function
- * this program prints "Programming is like a multibiligual puzzel
  * Return: 0 after printing the function
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
 }
