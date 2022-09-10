5bcfa69aa39088a7fe31e8fc8cdd045b97aba06c
augustus-fx <agbolanaugustine@gmail.com>
2022-09-10 06:32:26 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; < 26; 1++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\');
+	return (0)
+}
