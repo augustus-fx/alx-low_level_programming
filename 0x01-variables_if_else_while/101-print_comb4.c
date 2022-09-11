028d26c5d433a27d2f91a5f14256c1bc02f6c2c7
augustus-fx <agbolanaugustine@gmail.com>
2022-09-10 18:29:51 -0700

    Task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 012 t0 789.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i;
+	int e;
+	int g;
+
+	i = 48;
+	e = 48;
+	g = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			g = 48;
+			while (g < 58)
+			{
+				if (e != i && e != g && i != g && e < i && i < g)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				g++;
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+
+}
