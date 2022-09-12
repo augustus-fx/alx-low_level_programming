bcdabafd6cb9b7aac54d1e6ea81aef85a5315743
augustus-fx <agbolanaugustine@gmail.com>
2022-09-12 04:06:57 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ *main - Prints numbers between 0 to 9.
  *
  *return: 0 after printiing the function
  */
-int main(void);
+int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)", sizeof(float));
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
 	return (0);
 }
-
