fb82da8930ed7ccd4637a68ee140446e1fe8e3c5
augustus-fx <agbolanaugustine@gmail.com>
2022-09-12 09:11:12 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a simple program to write a line of text, this time without the
+ * commands from previous examples
+ *
+ * Return: l when finished
+ */
+int main(void)
+{
+	char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" :
+
+	fwrite(b, sizeof(char), size(b) - l, stderr);
+	return (l);
+}
