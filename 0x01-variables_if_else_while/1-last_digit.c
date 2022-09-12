39ccda89b52586c45cdd69b021713ebc333440bf
augustus-fx <agbolanaugustine@gmail.com>
2022-09-12 11:50:34 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - a simple program that prints the last digit of number,
- * and whether it is greater than 6, between 6 and 0, or less than o
+ * main - a simple program to print the last digit of a number,
+ * and whether it is greater than 6, between 6 and 0, or less than zero
  *
  * Return: 0 on success
  **/
@@ -18,7 +18,7 @@ int main(void)
 	if (n % 10 > 5)
 		printf("%d is %d and is greater than 5\n", n, n % 10);
 	else if (n % 10 != 0)
-		printf("%d is %d and is less than 6 and not 0\n", n % 10);
+		printf("%d is %d and is less than 6 and not 0\n", n, n  % 10);
 	else
 		printf("%d is %d and is 0\n", n, n % 10);
 	return (0);
