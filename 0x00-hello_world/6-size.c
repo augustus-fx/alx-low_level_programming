9f8be4f853e5279a557b34e1a107aa76092bbe60
augustus-fx <agbolanaugustine@gmail.com>
2022-09-12 04:21:48 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- *main - Prints numbers between 0 to 9.
+ * main - Prints numbers between 0 to 9.
  *
- *return: 0 after printiing the function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+	int ;
 
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-
 	}
 	putchar('\n');
 	return (0);
