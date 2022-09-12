fb8d35ca84bcdc72a34a5497ce285b5f3775e58d
augustus-fx <agbolanaugustine@gmail.com>
2022-09-12 08:22:55 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9.
+ * main - print the string in the put function
  *
- * Return: Always 0 (Success)
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
+	char c;
 	int i;
+	long li;
+	long long lli;
+	float f;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
