5ea17c72e065e8050d0b92a2be6b372558dfae0a
augustus-fx <agbolanaugustine@gmail.com>
2022-09-12 10:50:15 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 {
 	char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(b, sizeof(char), size(b) - 1, stderr);
+	fwrite(b, sizeof(char), sizeof(b) - 1, stderr);
 	return(1);
 }
