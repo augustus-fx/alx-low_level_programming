58ce135cca25486f17648b2de3a973b201f2467d
augustus-fx <agbolanaugustine@gmail.com>
2022-09-13 18:24:42 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,9 +16,36 @@ void print_times_table(int n)
 	{
 		for (m = 0; m <= n; n++)
 		{
-			for (a = o; a <= n; a++)
-			{
-			{
+			for (a = 0; a <= n; a++)
 				p = n * a;
+
 				if (c == 0 && p < 10)
 				{
+					printf(", %d", p);
+				}
+				else if (c == 0 && p < 100)
+				{
+					printf(", %d", p);
+				}
+				else if (c == 0 && p < 1000)
+				{
+					printf(", %d", p);
+				}
+				else if (c == 0 && p >= 1000)
+				{
+					printf(",%d", p);
+				}
+				else
+				{
+					printf(",%d", p);
+				}
+				c = 0;
+			}
+			putchar(10);
+			c = 1;
+		}
+	}
+	else
+	{
+	}
+}
