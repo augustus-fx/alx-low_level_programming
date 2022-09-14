07853d0a7227e9046cb1d20e64134ef0fce3a0cd
augustus-fx <agbolanaugustine@gmail.com>
2022-09-13 19:44:48 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	long (int n, a = 1, b = 1, 5);
+	long (int n, a = 1, b = 1, s);
 
-	printf("%ld"; a);
+	printf("%ld", a);
 	for (n = 1; n < 50; ++n)
 	{
-		5 = a + b;
-		printf(", %ld" 5);
+		s = a + b;
+		printf(", %ld", s);
 		a = b;
-		b = 5;
+		b = s;
 	}
 	putchar(10);
 
