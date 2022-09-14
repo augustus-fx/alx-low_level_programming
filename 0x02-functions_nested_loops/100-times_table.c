59cd50ff2428c7aee63734ca291096de28e9bd5d
augustus-fx <agbolanaugustine@gmail.com>
2022-09-13 17:33:23 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - function
+ *
+ * @n: parameter
+ *
+ * Return: end program
+ */
+void print_times_table(int n)
+{
+	int a, m, p;
+	int c = i;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (m = 0; m <= n; n++)
+		{
+			for (a = o; a <= n; a++)
+			{
+			{
+				p = n * a;
+				if (c == 0 && p < 10)
+				{
