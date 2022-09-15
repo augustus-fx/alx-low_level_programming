3acf7b0e60bd2ca1b19a568806b01e46a472e3de
augustus-fx <agbolanaugustine@gmail.com>
2022-09-14 19:01:29 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 {
 if (month >= 3 && day >= 60)
 {
-day++
+day++;
 }
 
 printf("Day of the year: %d\n", day);
@@ -28,7 +28,8 @@ if (month == 2 && day == 60)
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
-{printf("Day of the year: %d\n", day);
+{
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
