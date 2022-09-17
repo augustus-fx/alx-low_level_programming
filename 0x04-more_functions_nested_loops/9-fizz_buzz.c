8f5c1b199f564aa555d53ab04f652b82cc4be8e8
augustus-fx <agbolanaugustine@gmail.com>
2022-09-16 18:05:38 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main - prints the numbers from 1  to 100
+ * main - prints the numbers from 1 to 100
  * 3 multiples print Fizz instead of the number
- * 5 mutiples print Bus instead of the number
+ * 5 multiples print Bus instead of the number
  * 3 & 5 multiples print FizzBuzz instead of the number
  * Return: Always 0 (Success)
  */
