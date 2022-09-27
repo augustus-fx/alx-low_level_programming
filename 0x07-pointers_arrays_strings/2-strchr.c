ca665c9c27f9265ce0e6fd9ed1e1089d5c9b1cbf
augustus-fx <agbolanaugustine@gmail.com>
2022-09-27 16:56:05 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - Locates a character in a string
  * @s: String
  * @c: Character to search
- * Return: Pointer to the first occurence of the character c in the string s
+ * Return: Pointer to the first occurrence of the character c in the string s
  **/
 
 char *_strchr(char *s, char c)
