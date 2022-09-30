a25ce3a11c8f1456e40984668ad546d92c0cd32e
augustus-fx <agbolanaugustine@gmail.com>
2022-09-30 16:19:21 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.>
+
+/**
+ * main -> this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
