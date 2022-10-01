6c24c95c0ffe9bb1190e8ed13617fa4c0a5356b9
augustus-fx <agbolanaugustine@gmail.com>
2022-10-01 15:10:46 -0700

    multiplication of two numbers rectified again

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		num_1 = atoi(argv[1]);
 		num_2 = atoi(argv[2]);
 		ml = num_1 * num_2;
-		priintf("%d\n", mul);
+		printf("%d\n", mul);
 	}
-	Return (0);
+	Return(0);
 }
