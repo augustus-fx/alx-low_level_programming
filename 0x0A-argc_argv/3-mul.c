4d868f047e0357341e1e6278cbff4278f4b38b07
augustus-fx <agbolanaugustine@gmail.com>
2022-10-01 15:27:26 -0700

    mul of two numbers still rectified

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int num_1, num_2, mul;
 
-	if (arg != 3)
+	if (argc != 3)
 		printf("Error\n");
 	else
 	{
@@ -19,5 +19,5 @@ int main(int argc, char *argv[])
 		mul = num_1 * num_2;
 		printf("%d\n", mul);
 	}
-	Return(0);
+	return (0);
 }
