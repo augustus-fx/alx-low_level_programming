c159c1b29ff5da7f2c81f4e8437ce1f7d3626b5f
augustus-fx <agbolanaugustine@gmail.com>
2022-10-03 19:29:26 -0700

    An array of chars with specific chars - 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - creates space from heap and assigning
+ * a character to it
+ * @size: unsigned int
+ * @c: string of characters to be inputted
+ * Return: ptr
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *ptr;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = (char *)malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
+	return (ptr);
+}
