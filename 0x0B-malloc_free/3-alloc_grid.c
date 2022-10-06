f9d66635889f95d96ce1d7ccf0262bfaa0e4bdd8
augustus-fx <agbolanaugustine@gmail.com>
2022-10-05 20:29:05 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,22 +19,24 @@ int **alloc_grid(int width, int height)
 	else
 	{
 		p = (int **) malloc(height * sizeof(int *));
-		/* we have to make a malloc per pointer*/
+
 		if (!p)
 		{
 			free(p);
 			return (NULL);
 		}
 		for (i = 0; i < height; i++)
-			p[i] = (int *) malloc(width * sizeof(int));
-		if (!p[i])
 		{
-			for (j = 0; j <= i; j++)
-				free(p[j]);
-			free(p);
-			return (NULL);
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
 		}
-		for (a = 0; a < height; a++)
+		for (a = 0; a < width; a++)
 		{
 			for (b = 0; b < width; b++)
 			{
@@ -44,3 +46,4 @@ int **alloc_grid(int width, int height)
 		return (p);
 	}
 }
+
