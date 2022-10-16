365a750511d5cde0ffd0e463a6023c47266399d7
augustus-fx <agbolanaugustine@gmail.com>
2022-10-16 15:10:07 -0700

    Initializes a variable type srtuct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,21 @@
-#ifndef STRTYP
-#define STRTYP
-
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct_dog - structure of a dog.
- * @name: name of dog.
- * @age: age of dog.
- * @owner: owner of the dog.
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
 	char *name;
-	float *age;
-	char*owner;
+	float age;
+	char  *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -21,4 +23,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*STRTYP*/
+#endif /* DOG-H */
